5407bb355231ee7dc8c24a77b5699ad582994f10
KAISTChangmin <lcm914@kaist.ac.kr>
2023-10-25 17:52:07 +0000

    modify preprocessing

---
+++ scripts/crop_and_resize.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+import numpy as np
+import argparse
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--src_root", type=str, required=True)
+parser.add_argument("--dst_root", type=str, required=True)
+parser.add_argument("--desired_size", type=int, default=512)
+parser.add_argument("--num_data", type=int, default=200)
+
+args = parser.parse_args()
+
+os.makedirs(args.dst_root, exist_ok=True)
+fnames = sorted(os.listdir(args.src_root))
+idx = np.linspace(0, len(fnames)-1, args.num_data).astype(np.int32)
+fnames = [f for i, f in enumerate(fnames) if i in idx]
+
+for f in tqdm(fnames):
+    img = cv2.imread(os.path.join(args.src_root, f))
+    H, W = img.shape[:2]
+    L = min(H, W)
+
+    if H != W:
+        Hc, Wc = (H-L)//2, (W-L)//2
+        img = img[Hc:Hc+L, Wc:Wc+L]
+    
+    if L > args.desired_size:
+        img = cv2.resize(img, dsize=(args.desired_size,args.desired_size))
+    
+    cv2.imwrite(os.path.join(args.dst_root, f), img)
--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -2,12 +2,75 @@ if [ -z $DATA ]; then
     DATA=custom
 fi
 
-cd data/$DATA
+if [ -z $VIDEO ]; then
+    VIDEO=custom.mp4
+fi
 
-python ../../scripts/colmap2nerf.py \
---run_colmap \
---images ./images/ \
---out ./transforms.json \
---overwrite
+if [ ! -f "data/$DATA/original.mp4" ]; then
+    if [ ! -f $VIDEO ]; then
+        echo "$VIDEO doesn't exists"
+        exit 1
+    fi
+fi
 
-cd ../..
+if [ ! -d "data/$DATA" ]; then
+    mkdir -p data/$DATA
+fi
+
+###### 1. Copy Original Video to the data directory ######
+if [ ! -f "data/$DATA/original.mp4" ]; then
+    mv $VIDEO data/$DATA/original.mp4
+fi
+##########################################################
+
+
+########### 2. Unpack all frames of the video ############
+if [ ! -d "data/$DATA/original_images" ]; then
+    mkdir -p data/$DATA/temp
+    ffmpeg -i data/$DATA/original.mp4 data/$DATA/temp/%04d.png
+    mv data/$DATA/temp data/$DATA/original_images
+fi
+##########################################################
+
+
+##### 3. Uniformly Sample, Crop and Resize the Image #####
+if [ ! -d "data/$DATA/images" ]; then
+    mkdir -p data/$DATA/temp
+    python scripts/crop_and_resize.py \
+    --src_root data/$DATA/original_images \
+    --dst_root data/$DATA/temp \
+    --num_data 200
+    mv data/$DATA/temp data/$DATA/images
+fi
+##########################################################
+
+
+######## 4. Run Colmap and Convert to Text Format ########
+if [ ! -f "data/$DATA/colmap_text/images.txt" ]; then
+    colmap automatic_reconstructor \
+    --workspace_path data/$DATA \
+    --image_path data/$DATA/images \
+    --camera_model OPENCV \
+    --single_camera 1 \
+    --sparse 1 \
+    --dense 0
+
+    mkdir data/$DATA/colmap_text
+    
+    colmap model_converter \
+    --input_path data/$DATA/sparse/0 \
+    --output_path data/$DATA/colmap_text \
+    --output_type TXT
+fi
+##########################################################
+
+
+############# 5. Preprocess ###############
+if [ ! -f "data/$DATA/transforms.json" ]; then
+    python scripts/colmap2nerf.py \
+    --images data/$DATA/images \
+    --text data/$DATA/colmap_text \
+    --out data/$DATA/transforms.json \
+    --overwrite
+fi
+##########################################################
